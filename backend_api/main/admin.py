bd788cee371c8bc6099b76ff999ceb6adec19416
kariukijoni <kariukijoni@gmail.com>
2023-09-26 15:38:32 +0300

    Added footer

---
+++ backend_api/main/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from main.models import Vendor, Product, ProductCategory, Customer, Order, OrderItems, CustomerAddress, ProductRating
+# Register your models here.
+
+admin.site.register(Vendor)
+admin.site.register(Product)
+admin.site.register(ProductCategory)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(OrderItems)
+admin.site.register(CustomerAddress)
+admin.site.register(ProductRating)
