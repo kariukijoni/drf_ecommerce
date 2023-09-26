97e1123c9daa947820449391d30e4846205ef79c
kariukijoni <kariukijoni@gmail.com>
2023-09-26 14:12:06 +0300

    Added product categories API

---
+++ backend_api/main/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+# vendors models
+
+# vendor
+
+
+class Vendor(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.TextField(null=True)
+
+    def __str__(self):
+        return self.user.username
+
+# category
+
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Product Categories'
+
+# product
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        ProductCategory, on_delete=models.SET_NULL, null=True, related_name='category_product')
+    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
+
+# customer
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    mobile = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.user.username
+
+# ordermodel
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='customer_orders')
+    order_time = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return self.customer
+
+    def __unicode__(self):
+        return '%s' % (self.order_time)
+
+
+# orderitems
+
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='order_items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.title
+
+    class Meta:
+        verbose_name_plural = 'Order Items'
+
+# customerAddress
+
+
+class CustomerAddress(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='customer_addresses')
+    address = models.TextField()
+    default_address = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.address
+
+    class Meta:
+        verbose_name_plural = 'Customer Address'
+
+
+class ProductRating(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='rating_customers')
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name='product_ratings')
+    rating = models.IntegerField()
+    reviews = models.TextField()
+    add_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.rating}-{self.reviews}'
---
+++ backend_api/main/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from main.serializers import (VendorSerializer, VendorDetailSerializer,
+                              ProductSerializer, ProductDetailSerializer,
+                              CustomerSerializer, CustomerDetailSerializer,
+                              OrderSerializer, OrderDetailSerializer,
+                              CustomerAddressSerializer, ProductRatingSerializer,
+                              CategorySerializer, CategoryDetailSerializer)
+from main.models import Vendor, Product, Customer, Order, OrderItems, CustomerAddress, ProductRating, ProductCategory
+from rest_framework import generics, permissions, viewsets
+# Create your views here.
+
+
+class VendorList(generics.ListCreateAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+    # permission_classes=[permissions.IsAuthenticated]
+
+
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
+
+
+class CustomerList(generics.ListCreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+    # permission_classes=[permissions.IsAuthenticated]
+
+
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerDetailSerializer
+
+
+class OrderList(generics.ListCreateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    # permission_classes=[permissions.IsAuthenticated]
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    # queryset = OrderItems.objects.all()
+    serializer_class = OrderDetailSerializer
+
+    def get_queryset(self):
+        order_id = self.kwargs['pk']
+        order = Order.objects.get(id=order_id)
+        order_items = OrderItems.objects.filter(order=order)
+
+        return order_items
+
+
+class CustomerAddressViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomerAddressSerializer
+    queryset = CustomerAddress.objects.all()
+
+
+class ProductRatingViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductRatingSerializer
+    queryset = ProductRating.objects.all()
+
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProductCategory.objects.all()
+    serializer_class = CategoryDetailSerializer
